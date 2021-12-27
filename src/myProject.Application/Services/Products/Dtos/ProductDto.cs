d12241daa35a674606e2e8cf672c2fb2290722d8
MrVuThanhCong <mr.vtcsharp@gmail.com>
2021-12-27 16:45:20 +0700

    insert new product

---
+++ src/myProject.Application/Services/Products/Dtos/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using Abp.AutoMapper;
+using myProject.Data.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myProject.Services.Products.Dtos
+{
+    [AutoMapTo(typeof(Product))]
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        public string Image { get; set; }
+    }
+}
---
+++ src/myProject.Application/Services/Products/Reponses/ProductResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myProject.Services.Products.Reponses
+{
+    public class ProductResponse
+    {
+    }
+}
