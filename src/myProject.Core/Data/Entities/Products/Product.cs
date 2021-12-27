7b23c61c3862177600a0dbd5876bc32bbbfaf2b0
MrVuThanhCong <mr.vtcsharp@gmail.com>
2021-12-27 16:09:36 +0700

    add product entity

---
+++ src/myProject.Core/Data/Entities/Products/Product.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myProject.Data.Entities.Products
+{
+    public class Product : FullAuditedEntity
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Image { get; set; }
+    }
+}
--- src/myProject.EntityFrameworkCore/Migrations/20211227090645_Add-Product-Entity.Designer.cs
+++ src/myProject.EntityFrameworkCore/Migrations/20211227090645_Add-Product-Entity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using myProject.EntityFrameworkCore;
 
 namespace myProject.Migrations
 {
     [DbContext(typeof(myProjectDbContext))]
-    partial class myProjectDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211227090645_Add-Product-Entity")]
+    partial class AddProductEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
